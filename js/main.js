335e816f3a2d308105d2ad25ea310cbb7931095b
luoye <386617473@qq.com>
2016-07-15 23:12:14 +0800

    实现摸态框异步调用

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         // 阻止a标签的默认事件，组织冒泡
         event.preventDefault();
         event.stopPropagation();
-        // 获取a标签的href信息
+        // 获取a标签的data-html信息
         var href = $(this).attr('data-html');
         // ajax异步获取html信息并放入模态框的modal-body中
         $('#product-detail .modal-body').load(href, function () {
